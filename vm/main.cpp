eb8e043a229e0640a5f2c283b59dc905b7be6216
Shauray <shauray9@gmail.com>
2021-04-30 02:15:44 -0400

    added support for mul, sub, div

--- vm/main.cpp
+++ vm/main.cpp
@@ -1,13 +1,13 @@
-
 #include "stack-vm.h"
 
 using namespace std;
 
 int main(){
   StackVM vm;
-  vector<i32> prog{3, 4, 0x40000001, 0x40000000};
+  vector<i32> prog{3, 4, 0x40000001, 5, 0x40000002, 3, 0x40000003, 2, 0x40000004, 0x40000000};
 
   vm.loadProgram(prog);
+  
   vm.run();
   return 0;
 }
